3844ea8e4eef47274c0aefe6c79c14af4c1414d4
YashKumar <149514345+yashkumar2704@users.noreply.github.com>
2026-02-17 10:44:40 +0530

    Time: 1 ms (81.3%), Space: 42.8 MB (74.28%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String p, String s) {
+        HashMap<Character,String>h1=new HashMap<>();
+        HashMap<String,Character>h2=new HashMap<>();
+        String str[]=s.split(" ");
+        if(str.length!=p.length())return false;
+        for(int i=0;i<str.length;i++){
+            if(h1.containsKey(p.charAt(i))){
+            if(!h1.get(p.charAt(i)).equals(str[i]))return false;
+            }
+            else h1.put(p.charAt(i),str[i]);
+        }
+        
+        for(int i=0;i<str.length;i++){
+            if(h2.containsKey(str[i])){
+                if(h2.get(str[i])!=p.charAt(i))return false;
+            }
+            else h2.put(str[i],p.charAt(i));
+        }
+        return true;
+    }
+}
