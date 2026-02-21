5a6caebebc91470cba6ca47e53578070ee67879c
YashKumar <149514345+yashkumar2704@users.noreply.github.com>
2026-02-21 14:23:08 +0530

    Time: 2 ms (99.32%), Space: 77.6 MB (27.54%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,8 +3,11 @@ class Solution {
         int r=h.length-1;
         int ans=0;
         int lf=0;
+        int max_h=0;
+        for(int i=0;i<h.length;i++)max_h=Math.max(h[i],max_h);
         while(lf<r){
             ans=Math.max(ans,Math.min(h[lf],h[r])*(r-lf));
+            if(ans>max_h*(r-lf))return ans;
             if(h[lf]>h[r])r--;
             else lf++;
         }
