07a9467cf9b70133ac785a79f16c3f0e5301b54b
YashKumar <149514345+yashkumar2704@users.noreply.github.com>
2026-03-01 18:00:54 +0530

    Time: 16 ms (91.81%), Space: 45.5 MB (82.35%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,26 +1,28 @@
 class Solution {
     public int threeSumClosest(int[] nums, int t) {
-        int ans=Integer.MAX_VALUE;
-        int an=Integer.MIN_VALUE;
-        int c=0;
+        
         Arrays.sort(nums);
+        int an=nums[0]+nums[1]+nums[2];
+        int c=0;
+
         for(int i=0;i<nums.length-1;i++){
-            int a=nums[i];
+            
             int l=i+1;
             int r=nums.length-1;
-            while(r>1 && l<nums.length-1){
-                if(l==r){r--;continue;};
-                c=nums[l]+nums[r]+a;
-                if(nums[l]+nums[r]+a==t)return c;
-                else if(nums[l]+nums[r]+a<t)l++;
+
+            while(l<r){
+                c= nums[i] + nums[l] + nums[r];
+
+                if(c==t)return c;
+
+                else if(c<t)l++;
                 
                 else r--;
-                if((c>t && c<ans))ans=c;
-                if((c<t && c>an))an=c;
+
+                if(Math.abs(c-t)<Math.abs(an-t))an=c;
             }
         }
-        // System.out.println(Math.abs(ans-t)+"   "+ Math.abs(an-t));
-        if(Math.abs(ans-t)>Math.abs(an-t) && an!=Integer.MIN_VALUE)return an;
-        return ans;
+        
+        return an;
     }
 }
